b5b4b5e5df0d41407832afc2a6e984f9ccf15ce0
idbaali <ikebaalidavid@live.de>
2023-02-06 11:57:48 +0100

    MyBigCompany

---
+++ phpDemo/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <title>Ma première page PHP</title>
+   </head>
+
+   <body>
+      <?php
+         echo 'Bonjour';
+      ?>
+   </body>
+</html>0
